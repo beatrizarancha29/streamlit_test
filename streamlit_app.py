a3c5ebb859cfdc7d1143f643df6571d274ab59a2
AbdullahUPC <151859134+AbdullahUPC@users.noreply.github.com>
2023-12-05 15:39:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,6 @@ def get_weather_data(city):
     return response.json()['forecast']['forecastday']
 
 # Dummy data for illustration (replace this with your actual data)
-temperature_data = pd.DataFrame({
-    'date': pd.date_range(start='2023-12-01', periods=30),
-    'temperature': np.random.uniform(50, 70, size=30),
-})
-
 electricity_prices = np.random.uniform(0.10, 0.50, size=30)
 
 # Page setting
@@ -34,12 +29,13 @@ a3.metric("Humidity", "-", "-")  # Replace with actual data or remove if not nee
 # Row B
 b1, b2, b3, b4 = st.columns(4)
 
-# Fetch weather data for the first day
-city_name = "Barcelona"  # You can make it dynamic based on user input
+# Fetch weather data for the entire month
+city_name = "New York"  # You can make it dynamic based on user input
 weather_data = get_weather_data(city_name)
-first_day_temperature = weather_data[0]['day']['avgtemp_c']
+temperatures = [day['day']['avgtemp_c'] for day in weather_data]
 
-b1.metric("Temperature", f"{first_day_temperature:.2f} °C", "-")
+# Update the Temperature metric to display the entire month's forecast
+b1.metric("Temperature", f"Min: {min(temperatures):.2f} °C, Max: {max(temperatures):.2f} °C", "-")
 
 # Continue with existing metrics
 b2.metric("Electricity Price", f"{electricity_prices[0]:.2f} $/kWh", "-")
@@ -51,7 +47,7 @@ c1, c2 = st.columns((7, 3))
 with c1:
     st.markdown('### Temperature Trend')
     # Replace this with your actual line chart or visualization for temperature
-    st.line_chart(temperature_data.set_index('date')['temperature'])
+    st.line_chart(pd.DataFrame({'Temperature': temperatures}, index=pd.date_range(start='2023-12-01', periods=30)))
 with c2:
     st.markdown('### Electricity Price Trend')
     # Replace this with your actual line chart or visualization for electricity prices
@@ -63,12 +59,12 @@ with d1:
     st.markdown('### Combined Trends')
     # Replace this with your actual line chart or visualization for both temperature and electricity prices
     combined_data = pd.DataFrame({
-        'Temperature': temperature_data['temperature'],
+        'Temperature': temperatures,
         'Electricity Price': electricity_prices,
-    }, index=temperature_data['date'])
+    }, index=pd.date_range(start='2023-12-01', periods=30))
     st.line_chart(combined_data)
 with d2:
     st.markdown('### Statistics')
     # Replace this with any statistics or summary you want to display
-    st.text(f"Average Temperature: {temperature_data['temperature'].mean():.2f} °C")
+    st.text(f"Average Temperature: {np.mean(temperatures):.2f} °C")
     st.text(f"Average Electricity Price: {np.mean(electricity_prices):.2f} $/kWh")
