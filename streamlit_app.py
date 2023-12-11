838f29fb44a18fa8b5c82496f4b8080211ff4cc1
AbdullahUPC <151859134+AbdullahUPC@users.noreply.github.com>
2023-12-11 02:00:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import datetime
 import matplotlib.pyplot as plt
 
 # Function to get weather data from the external API
-def get_weather_data(city):
+def get_weather_data(city, days=7):
     api_key = '46b2788544324cc8ada143152230512'  # Replace with your actual weather API key
-    response = requests.get(f'https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=30')
+    response = requests.get(f'https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days={days}')
     
     try:
         response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
@@ -65,41 +65,53 @@ a3.metric("Humidity", "-", "-")  # Replace with actual data or remove if not nee
 # Row B
 b1, b2, b3, b4 = st.columns(4)
 
-# Fetch weather data for the entire month
-city_name = "New York"  # You can make it dynamic based on user input
-weather_data = get_weather_data(city_name)
+# Fetch weather data for the entire week
+city_name = "Barcelona"  # Barcelona, Spain
+weather_data_week = get_weather_data(city_name, days=7)
 
-# Extract temperature and date data from the API response
-temperatures = [day['day'].get('avgtemp_c') for day in weather_data if 'day' in day and 'avgtemp_c' in day['day']]
-dates = [datetime.datetime.strptime(day['date'], '%Y-%m-%d') for day in weather_data if 'date' in day]
+# Extract temperature and date data from the API response for the week
+temperatures_week = [day['day'].get('avgtemp_c') for day in weather_data_week if 'day' in day and 'avgtemp_c' in day['day']]
+dates_week = [datetime.datetime.strptime(day['date'], '%Y-%m-%d') for day in weather_data_week if 'date' in day]
 
-# Update the Temperature metric to display the entire month's forecast
-if temperatures:
-    b1.metric("Temperature", f"Min: {min(temperatures):.2f} °C, Max: {max(temperatures):.2f} °C", "-")
+# Update the Temperature metric to display the entire week's forecast
+if temperatures_week:
+    b1.bar_chart(pd.DataFrame({'Temperature': temperatures_week}, index=dates_week))
 else:
-    b1.warning("Temperature data not available")
+    b1.warning("Temperature data not available for the week")
+
+# Fetch weather data for the entire day
+weather_data_day = get_weather_data(city_name, days=1)
+
+# Extract temperature and date data from the API response for the day
+temperatures_day = [hour['hour'].get('temp_c') for hour in weather_data_day[0]['hour']]
+hours_day = [datetime.datetime.strptime(hour['time'], '%Y-%m-%d %H:%M') for hour in weather_data_day[0]['hour']]
+
+# Update the Temperature metric to display the entire day's forecast
+if temperatures_day:
+    b2.bar_chart(pd.DataFrame({'Temperature': temperatures_day}, index=hours_day))
+else:
+    b2.warning("Temperature data not available for the day")
 
 # Continue with existing metrics
-b2.metric("Electricity Price", f"{round(min(0.10, 0.50), 2)} - {round(max(0.10, 0.50), 2)} $/kWh", "-")
-b3.metric("Humidity", "-", "-")  # Replace with actual data or remove if not needed
+b3.metric("Electricity Price", f"{round(min(0.10, 0.50), 2)} - {round(max(0.10, 0.50), 2)} $/kWh", "-")
 b4.metric("Humidity", "-", "-")  # Replace with actual data or remove if not needed
 
 # Row C
 c1, c2 = st.columns((7, 3))
 with c1:
-    st.markdown('### Temperature Trend')
-    if temperatures:
-        st.line_chart(pd.DataFrame({'Temperature': temperatures}, index=dates))
+    st.markdown('### Temperature Trend for the Day')
+    if temperatures_day:
+        st.line_chart(pd.DataFrame({'Temperature': temperatures_day}, index=hours_day))
     else:
-        st.warning("Temperature data not available")
+        st.warning("Temperature data not available for the day")
 with c2:
-    st.markdown('### Electricity Price Trend')
+    st.markdown('### Electricity Price Trend for the Day')
 
     # Get real-time electricity prices
     real_time_prices, real_time_times = get_electricity_prices()
 
     if real_time_prices:
-        # Plot the real-time electricity prices
+        # Plot the real-time electricity prices for the day
         df_prices = pd.DataFrame({'Time': real_time_times, 'Price': real_time_prices})
         df_prices['Time'] = pd.to_datetime(df_prices['Time'])
         
@@ -107,22 +119,22 @@ with c2:
         ax.plot(df_prices['Time'], df_prices['Price'], label='Real-time Prices', marker='o')
         ax.set_xlabel('Time')
         ax.set_ylabel('Electricity Price ($/kWh)')
-        ax.set_title('Real-time Electricity Price Variation')
+        ax.set_title('Real-time Electricity Price Variation for the Day')
         ax.tick_params(rotation=45)
         ax.legend()
         st.pyplot(fig)
     else:
-        st.warning("Real-time electricity price data not available")
+        st.warning("Real-time electricity price data not available for the day")
 
 # Row D
 d1, d2 = st.columns((7, 3))
 with d1:
     st.markdown('### Statistics')
     # Replace this with any statistics or summary you want to display
-    if temperatures:
-        st.text(f"Average Temperature: {round(np.mean(temperatures), 2)} °C")
-        st.text(f"Average Electricity Price: {round(np.mean(real_time_prices), 2)} $/kWh")
+    if temperatures_week:
+        st.text(f"Weekly Average Temperature: {round(np.mean(temperatures_week), 2)} °C")
+        st.text(f"Weekly Average Electricity Price: {round(np.mean(real_time_prices), 2)} $/kWh")
     else:
-        st.warning("Statistics not available")
+        st.warning("Statistics not available for the week")
 with d2:
     pass  # Removed the content of the second column (previously combined trend)
