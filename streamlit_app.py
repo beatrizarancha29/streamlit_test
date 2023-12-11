636b33af8a459aa278a3ff5f4cf38a145df31b5e
AbdullahUPC <151859134+AbdullahUPC@users.noreply.github.com>
2023-12-11 02:12:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,48 +6,29 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
-# Function to get weather data from the external API
-def get_weather_data(city, days=7):
-    api_key = '46b2788544324cc8ada143152230512'  # Replace with your actual weather API key
-    response = requests.get(f'https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days={days}')
-    
+# Function to get weather data from the OpenWeatherMap API
+def get_weather_data(api_key, city):
+    endpoint = 'http://api.openweathermap.org/data/2.5/forecast'
+    params = {'q': city, 'appid': api_key}
+
     try:
+        response = requests.get(endpoint, params=params)
         response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
-        return response.json()['forecast']['forecastday']
+        return response.json()['list']
     except requests.exceptions.RequestException as e:
         st.error(f"Error getting weather data: {e}")
         return []
 
-# Function to get real-time electricity prices from the external API with hourly increments
+# Function to get real-time electricity prices (simulated data)
 def get_electricity_prices():
-    endpoint = 'https://apidatos.ree.es'
-    get_archives = '/es/datos/mercados/precios-mercados-tiempo-real'
-    headers = {'Accept': 'application/json',
-               'Content-Type': 'application/json',
-               'Host': 'apidatos.ree.es'}
-
     now = datetime.datetime.now()
-    start_date = now.strftime('%Y-%m-%dT%H:%M')
-    end_date = (now + datetime.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M')  # 24 hours from now
+    start_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_time = start_time + datetime.timedelta(hours=23, minutes=59)
 
-    params = {'start_date': start_date, 'end_date': end_date, 'time_trunc': 'hour'}
+    times = pd.date_range(start=start_time, end=end_time, freq='H')
+    prices = np.random.uniform(0.1, 0.5, len(times))
 
-    try:
-        response = requests.get(endpoint + get_archives, headers=headers, params=params)
-        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
-        data = response.json()
-
-        if 'included' in data:
-            prices = [item['attributes']['values'][0]['value'] for item in data['included']]
-            times = [item['attributes']['values'][0]['datetime'] for item in data['included']]
-            return prices, times
-        else:
-            st.warning("Electricity price data not available")
-            return [], []
-
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error making API request: {e}")
-        return [], []
+    return prices, times
 
 # Page setting
 st.set_page_config(layout="wide")
@@ -65,13 +46,14 @@ a3.metric("Humidity", "-", "-")  # Replace with actual data or remove if not nee
 # Row B
 b1, b2, b3, b4 = st.columns(4)
 
-# Fetch weather data for the entire week
-city_name = "Barcelona"  # Barcelona, Spain
-weather_data_week = get_weather_data(city_name, days=7)
+# Fetch weather data for the next 7 days using OpenWeatherMap API
+city_name = "Barcelona"
+openweathermap_api_key = "84e73656e11445cd7ddcc77a98850373"  # Replace with your actual API key
+weather_data_week = get_weather_data(openweathermap_api_key, city_name)
 
 # Extract temperature and date data from the API response for the week
-temperatures_week = [day['day'].get('avgtemp_c') for day in weather_data_week if 'day' in day and 'avgtemp_c' in day['day']]
-dates_week = [datetime.datetime.strptime(day['date'], '%Y-%m-%d') for day in weather_data_week if 'date' in day]
+temperatures_week = [item['main']['temp'] for item in weather_data_week]
+dates_week = [datetime.datetime.fromtimestamp(item['dt']) for item in weather_data_week]
 
 # Update the Temperature metric to display the entire week's forecast
 if temperatures_week:
@@ -79,23 +61,14 @@ if temperatures_week:
 else:
     b1.warning("Temperature data not available for the week")
 
-# Fetch weather data for the entire day
-weather_data_day = get_weather_data(city_name, days=1)
-
-# Extract temperature and date data from the API response for the day
-try:
-    temperatures_day = [hour['hour'].get('temp_c') for hour in weather_data_day[0]['hour']]
-    hours_day = [datetime.datetime.strptime(hour['time'], '%Y-%m-%d %H:%M') for hour in weather_data_day[0]['hour']]
-except (KeyError, TypeError):
-    st.warning("Temperature data not available for the day")
-    temperatures_day = []
-    hours_day = []
+# Fetch electricity prices for the next 24 hours (simulated data)
+real_time_prices, real_time_times = get_electricity_prices()
 
 # Update the Temperature metric to display the entire day's forecast
-if temperatures_day:
-    b2.bar_chart(pd.DataFrame({'Temperature': temperatures_day}, index=hours_day))
+if real_time_prices.any():
+    b2.line_chart(pd.DataFrame({'Electricity Price': real_time_prices}, index=real_time_times))
 else:
-    b2.warning("Temperature data not available for the day")
+    b2.warning("Electricity price data not available for the day")
 
 # Continue with existing metrics
 b3.metric("Electricity Price", f"{round(min(0.10, 0.50), 2)} - {round(max(0.10, 0.50), 2)} $/kWh", "-")
@@ -104,32 +77,13 @@ b4.metric("Humidity", "-", "-")  # Replace with actual data or remove if not nee
 # Row C
 c1, c2 = st.columns((7, 3))
 with c1:
-    st.markdown('### Temperature Trend for the Day')
-    if temperatures_day:
-        st.line_chart(pd.DataFrame({'Temperature': temperatures_day}, index=hours_day))
+    st.markdown('### Temperature Trend for the Week')
+    if temperatures_week:
+        st.line_chart(pd.DataFrame({'Temperature': temperatures_week}, index=dates_week))
     else:
-        st.warning("Temperature data not available for the day")
+        st.warning("Temperature data not available for the week")
 with c2:
     st.markdown('### Electricity Price Trend for the Day')
-
-    # Get real-time electricity prices
-    real_time_prices, real_time_times = get_electricity_prices()
-
-    if real_time_prices:
-        # Plot the real-time electricity prices for the day
-        df_prices = pd.DataFrame({'Time': real_time_times, 'Price': real_time_prices})
-        df_prices['Time'] = pd.to_datetime(df_prices['Time'])
-        
-        fig, ax = plt.subplots()
-        ax.plot(df_prices['Time'], df_prices['Price'], label='Real-time Prices', marker='o')
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Electricity Price ($/kWh)')
-        ax.set_title('Real-time Electricity Price Variation for the Day')
-        ax.tick_params(rotation=45)
-        ax.legend()
-        st.pyplot(fig)
-    else:
-        st.warning("Real-time electricity price data not available for the day")
 
 # Row D
 d1, d2 = st.columns((7, 3))
