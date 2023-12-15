5cade0b29839fddd9af8562ee00002910b7fc81b
AbdullahUPC <151859134+AbdullahUPC@users.noreply.github.com>
2023-12-15 05:49:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import webbrowser
 
 # Function to get weather data from the external API
-@st.cache(allow_output_mutation=True)
+@st.cache
 def get_weather_data(city):
     api_key = '46b2788544324cc8ada143152230512'  # Replace with your actual weather API key
     response = requests.get(f'https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=30')
     return response.json()['forecast']['forecastday']
 
 # Function to get the electricity prices for a specific day and hour
-@st.cache(allow_output_mutation=True)
+@st.cache
 def get_electricity_price_for_date(date, hour):
     # Actual values for endpoint, get_archives, and headers
     endpoint = 'https://apidatos.ree.es'
@@ -56,7 +56,7 @@ def get_electricity_price_for_date(date, hour):
         return None
 
 # Function to get data from the provided link
-@st.cache(allow_output_mutation=True)
+@st.cache
 def get_sensor_data():
     data_url = 'https://raw.githubusercontent.com/AbdullahUPC/ControlProject/main/hello.txt'
     response = requests.get(data_url)
@@ -73,4 +73,98 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# ... (unchanged)
+# Row A
+a1, a2, a3 = st.columns(3)
+a1.image(Image.open('autoprice.png'))
+
+# Display Temperature Sensor 1, Temperature Sensor 2, and LED Status
+sensor_data = get_sensor_data()
+if sensor_data:
+    lines = sensor_data.split(', ')
+    for line in lines:
+        if "Temperature 1" in line:
+            temp_sensor1 = line.split(': ')[1].replace('°K', '°C')
+            a2.metric("Temperature Sensor 1", temp_sensor1, "-")
+        elif "Temperature 2" in line:
+            temp_sensor2 = line.split(': ')[1].replace('°K', '°C')
+            a3.metric("Temperature Sensor 2", temp_sensor2, "-")
+        elif "LED Status" in line:
+            led_status = line.split(': ')[1]
+            a2.metric("LED Status", led_status, "-")
+
+# Row B
+b1, b2, b3, b4 = st.columns(4)
+
+# Fetch weather data for the entire month in Barcelona
+city_name = "Barcelona"  # Dynamically set to Barcelona
+weather_data = get_weather_data(city_name)
+
+# Extract temperature and date data from the API response
+temperatures = [day['day'].get('avgtemp_c') for day in weather_data if 'day' in day and 'avgtemp_c' in day['day']]
+dates = [day['date'] for day in weather_data if 'date' in day]
+
+# Update the Temperature metric to display the entire month's forecast
+if temperatures:
+    b1.metric("Temperature", f"Min: {min(temperatures):.2f} °C, Max: {max(temperatures):.2f} °C", "-")
+else:
+    b1.warning("Temperature data not available")
+
+# Continue with existing metrics
+b2.metric("Electricity Price", f"{round(min(0.10, 0.50), 2)} - {round(max(0.10, 0.50), 2)} €/kWh", "-")
+b3.metric("LED Status", "-", "-")
+b4.metric("LED Status", "-", "-")
+
+# Row C
+c1, c2 = st.columns((7, 3))
+with c1:
+    st.markdown('### Temperature Trend')
+    if temperatures:
+        # Use an appropriate graph for temperature, e.g., line chart or area chart
+        st.line_chart(pd.DataFrame({'Temperature': temperatures}, index=dates))
+    else:
+        st.warning("Temperature data not available")
+
+# Row D
+# Fetch electricity prices for the previous day
+yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+previous_day = yesterday.strftime('%Y-%m-%d')
+hours_of_day = range(24)
+prices = []
+
+# Button to call the price API
+fetch_button_pressed = st.button("Fetch Electricity Prices")
+if fetch_button_pressed:
+    for hour in hours_of_day:
+        price = get_electricity_price_for_date(previous_day, hour)
+        print(f"The electricity price for {previous_day} {hour} is {price} €/kWh at {hour}:00.")
+        
+        # Append the price to the array
+        prices.append(price)
+
+d1, d2, d3 = st.columns((5, 5, 2))
+with d1:
+    st.markdown('### Electricity Price Trend')
+    if fetch_button_pressed:
+        plt.plot(hours_of_day, prices, marker='o')
+        plt.title(f'Electricity Prices on {previous_day}')
+        plt.xlabel('Hour of the Day')
+        plt.ylabel('Electricity Price (€/kWh)')
+        st.pyplot(plt)
+    else:
+        st.warning("Please press the 'Fetch Electricity Prices' button.")
+
+with d2:
+    st.markdown('### Statistics')
+    # Replace this with any statistics or summary you want to display
+    if fetch_button_pressed and temperatures and prices:
+        st.text(f"Average Temperature: {round(np.mean(temperatures), 2)} °C")
+        st.text(f"Average Electricity Price: {round(np.mean(prices), 2)} €/kWh")
+    elif fetch_button_pressed:
+        st.warning("Statistics not available")
+
+with d3:
+    pass  # Removed the content of the second column (previously combined trend)
+
+# Button to access the Receiver Statistics page
+if st.button("Receiver Statistics"):
+    webbrowser.open_new_tab("http://www.xyz.com")
